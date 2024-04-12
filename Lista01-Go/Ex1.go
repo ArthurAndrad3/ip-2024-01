63a62113549c0e0be21139baab6b6d91ea20dae0
ArthurAndrad3 <arthur-amf@homail.com>
2024-04-12 20:08:06 -0300

    ex 1

--- Lista01-Go/Ex1.go
+++ Lista01-Go/Ex1.go
@@ -1,1 +1,26 @@
-aa
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nota1, nota2, nota3 float64
+
+	fmt.Println("Digite a nota 1:")
+	fmt.Scanln(&nota1)
+	fmt.Println("Digite a nota 2:")
+	fmt.Scanln(&nota2)
+	fmt.Println("Digite a nota 3:")
+	fmt.Scanln(&nota3)
+
+	media := (nota1 + nota2 + nota3) / 3
+
+	fmt.Printf("Média: %.2f\n", media)
+
+	if media > 6 {
+		fmt.Println("Aprovado")
+	} else {
+		fmt.Println("Reprovado")
+	}
+}
